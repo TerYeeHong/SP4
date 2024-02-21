5a97af238ac747b6eab590f51d6f6ac0ca07917f
Eatennn <220195D@mymail.nyp.edu.sg>
2024-02-21 19:31:12 +0800

    stuff

---
+++ Assets/Ethan/LizardEnemyController.cs
@@ -0,0 +1,284 @@
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LizardEnemyController : EnemyUnit
+{
+
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform enemyTransform;
+    [SerializeField] private Transform movePositionTransform;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject[] players;
+    //[SerializeField] private PhotonView photonView;
+    //[SerializeField] private PhotonView photonView;
+    //[SerializeField] private Rigidbody rigidbody;
+
+    public float m_Thrust = 20f;
+    float distance;
+    float nearestDistance = 100;
+    bool attackBasic = false;
+    int invisTimer = 0;
+
+    public bool isInvisible = false;
+
+    [Header("Materials")]
+    public SkinnedMeshRenderer enemyRenderer;
+    public Material originalMat, xrayMat;
+
+    STATES CURRENT_STATE = STATES.IDLE;
+    enum STATES
+    {
+        IDLE,
+        WALKING,
+        GOINVIS,
+        DEAD,
+        ATTACK,
+        HIT
+
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        navMeshAgent.speed = speed_unit;
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        for (int i = 0; i < players.Length; i++)
+        {
+
+
+            distance = Vector3.Distance(enemyTransform.position, players[i].transform.position);
+            if (distance < nearestDistance)
+            {
+                //movePositionTransform = players[i].transform;
+
+                range_unit = Vector3.Distance(enemyTransform.position, movePositionTransform.position);
+            }
+
+
+
+            
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the collider belongs to an object tagged as "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Perform actions when the hitbox collider overlaps with the player
+            if (attackBasic)
+            {
+                Debug.Log("LIZARD ATTACK PLAYER.");
+            }
+
+
+            // You can perform additional actions here, such as dealing damage to the player or triggering events.
+        }
+    }
+
+    private void OnEnable()
+    {
+        RaiseEvents.SetInactiveEvent += Inactive;
+        RaiseEvents.SetActiveEvent += Active;
+    }
+
+    private void OnDisable()
+    {
+        RaiseEvents.SetInactiveEvent -= Inactive;
+        RaiseEvents.SetActiveEvent -= Active;
+    }
+
+    public void AttackBasicSTART()
+    {
+        attackBasic = true;
+    }
+
+    public void AttackBasicEND()
+    {
+        attackBasic = false;
+    }
+
+    public override void OnDeath()
+    {
+        base.OnDeath();
+        // animator.SetBool("IsDead", true);
+
+        CURRENT_STATE = STATES.DEAD;
+        if (photonView.IsMine)
+        {
+            Debug.Log("photonView.ViewID" + photonView.ViewID);
+            string sentData = "" + photonView.ViewID;
+            RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.MasterClient };
+            PhotonNetwork.RaiseEvent(RaiseEvents.ENEMYDIEEVENT, sentData, raiseEventOptions, SendOptions.SendReliable);
+        }
+
+        // GameEvents.m_instance.unitDied.Invoke(unit_type.name);
+        //Destroy(gameObject, 1.0f);
+    }
+
+    private void Update()
+    {
+
+    
+
+        //Debug.Log("ability active: "+ abilityXRay.xrayActive);
+        Debug.Log("current state: " + CURRENT_STATE);
+        Debug.Log("invis timer: " + invisTimer);
+
+
+        if (CURRENT_STATE != STATES.DEAD)
+        {
+
+            if (range_unit < 3)
+            {
+
+                CURRENT_STATE = STATES.ATTACK;
+              
+            }
+
+            else if (range_unit < 20 && CURRENT_STATE != STATES.GOINVIS)
+            {
+
+                CURRENT_STATE = STATES.WALKING;
+               
+            }
+            else if (movePositionTransform.position == navMeshAgent.destination)
+            {
+
+                CURRENT_STATE = STATES.IDLE;
+                //animator.SetBool("IsHit", false);
+            }
+
+            if (CURRENT_STATE == STATES.HIT)
+            {
+                animator.SetTrigger("IsHit");
+                //animator.SetBool("IsHit", true);
+                //CURRENT_STATE = STATES.IDLE;
+            }
+
+            else if (CURRENT_STATE == STATES.ATTACK)
+            {
+                animator.SetTrigger("IsAttacking");
+                isInvisible = false;
+
+            }
+            else if (CURRENT_STATE == STATES.IDLE)
+            {
+                animator.SetBool("IsMoving", false);
+            }
+
+            else if (CURRENT_STATE == STATES.GOINVIS)
+            {
+                invisTimer = 0;
+
+                isInvisible = true;
+
+                //CURRENT_STATE = STATES.WALKING;
+                //if (animator.GetCurrentAnimatorStateInfo(0).IsName("attack1"))
+                //{
+                //    CURRENT_STATE = STATES.WALKING;
+                //}
+            }
+            else if (CURRENT_STATE == STATES.WALKING)
+            {
+                invisTimer++;
+                if( invisTimer >= 10)
+                {
+                   
+                    CURRENT_STATE = STATES.GOINVIS;
+
+                }
+                animator.SetBool("IsMoving", true);
+                //moving = true;
+                speed_unit = 2;
+                navMeshAgent.speed = speed_unit;
+                navMeshAgent.destination = movePositionTransform.position;
+            }
+
+
+            if (isInvisible)
+            {
+                enemyRenderer.sharedMaterial = xrayMat;
+            }
+            else if (!isInvisible)
+            {
+                enemyRenderer.sharedMaterial = originalMat;
+            }
+
+        }
+        else if (CURRENT_STATE == STATES.DEAD)
+        {
+            Debug.LogWarning("HDIAHDIA");
+            animator.SetBool("IsDead", true);
+            enabled = false;
+            // navMeshAgent.destination = movePositionTransform.position;
+        }
+
+        Debug.LogWarning("CURRENT_STATE " + CURRENT_STATE);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("attack1"))
+        {
+            speed_unit = 0;
+            navMeshAgent.speed = speed_unit;
+        }
+
+        //animator.SetTrigger("IsHit");
+    }
+
+    public override bool TakeDamage(int damage)
+    {
+        //health_unit -= damage;
+        Debug.Log("GETTING MY ASS HIT");
+
+        Debug.Log("damage" + damage);
+        //Damage pop up
+        if (damage > 0)
+        {
+            Debug.Log("OUCHH");
+
+            // rigidbody.AddForce(transform.forward * m_Thrust);
+            CURRENT_STATE = STATES.HIT;
+            animator.SetTrigger("IsHit");
+
+
+
+        }
+
+        //if (health_unit <= 0)
+        //{
+        //    OnDeath();
+        //}
+
+        return base.TakeDamage(damage);
+    }
+
+    public void Inactive(string data)
+    {
+        Debug.Log("INACTIE CALLLED");
+        if ("" + data == "" + photonView.ViewID)
+        {
+            Debug.Log("setINactuve");
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void Active(string data)
+    {
+        Debug.Log("ACTIVEEEEEE CALLLED");
+        if ("" + data == "" + photonView.ViewID)
+        {
+            Debug.Log("setactuve");
+            gameObject.SetActive(true);
+        }
+    }
+
+}
+
--- Assets/Scripts/Ability/AbilityXRay.cs
+++ Assets/Scripts/Ability/AbilityXRay.cs
@@ -8,17 +8,18 @@ public class AbilityXRay : MonoBehaviour
     public KeyCode activationKey = KeyCode.X;
     public float cooldownDuration = 10f;
     public float abilityActive = 5f;
-    public TMP_Text cooldownText;
+    //public TMP_Text cooldownText;
     public SkinnedMeshRenderer enemyRenderer;
     private float dissolveSpeed = 2.5f;
-    private int enemyLayerIndex;
     private bool isEnemyLayerActive = false;
     private bool isCooldownActive = false;
     public AudioClip sfx;
-    private float dissolveAmt;
+    public float dissolveAmt;
+
+    public bool isXRayActive;
+    private float xRayActivationTime;
 
     public Material originalMat, xrayMat;
-
     void Start()
     {
         if (cam == null)
@@ -26,44 +27,46 @@ public class AbilityXRay : MonoBehaviour
             Debug.LogError("Camera not assigned!");
             return;
         }
+        
+        dissolveAmt = 1.2f;
+        isXRayActive = false;
 
-        enemyLayerIndex = LayerMask.NameToLayer("InvisEnemy");
-        DeactivateEnemyLayer(); // Deactivate enemy layer by default
-
-        dissolveAmt = 0.9f;
-        SetDissolveAmt(dissolveAmt);
-
-        enemyRenderer.sharedMaterial = originalMat;
-
-        cooldownText.gameObject.SetActive(false);
+    //cooldownText.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        if (!isCooldownActive && Input.GetKeyDown(activationKey) && !isEnemyLayerActive)
+        if (!isCooldownActive && Input.GetKeyDown(activationKey) && !isXRayActive)
         {
+            isXRayActive = true;
+            xRayActivationTime = Time.time;
+
             StartCoroutine(ActivateEnemyLayerForSeconds(abilityActive));
             StartCoroutine(CooldownCoroutine());
+        }
+
+        // Check if X-ray ability is active and elapsed time is less than abilityActive duration
+        if (isXRayActive && (Time.time - xRayActivationTime) < abilityActive)
+        {
+            SetDissolveAmt(-1.1f); // Set dissolve amount to -1.1f (visible)
         }
     }
 
     IEnumerator ActivateEnemyLayerForSeconds(float seconds)
     {
-        ActivateEnemyLayer();
         GameEvents.m_instance.playNewAudioClip.Invoke(sfx, AudioSfxManager.AUDIO_EFFECT.DEFAULT);
+
         // Start dissolve effect
         StartCoroutine(DissolveInEffect());
 
         yield return new WaitForSeconds(seconds);
+
 
         StartCoroutine(DissolveOutEffect());
     }
 
     IEnumerator DissolveInEffect()
     {
-        enemyRenderer.sharedMaterial = xrayMat;
-
-        dissolveAmt = 0.9f;
         while (dissolveAmt > -1.1f)
         {
             dissolveAmt -= Time.deltaTime * dissolveSpeed;
@@ -75,10 +78,7 @@ public class AbilityXRay : MonoBehaviour
 
     IEnumerator DissolveOutEffect()
     {
-        enemyRenderer.sharedMaterial = originalMat;
-
-        dissolveAmt = -1.1f;
-        while (dissolveAmt < 0.9f)
+        while (dissolveAmt < 1.2f)
         {
             dissolveAmt += Time.deltaTime * dissolveSpeed;
 
@@ -86,38 +86,25 @@ public class AbilityXRay : MonoBehaviour
             yield return null;
         }
 
-        DeactivateEnemyLayer();
+        isXRayActive = false;
     }
 
     IEnumerator CooldownCoroutine()
     {
         isCooldownActive = true;
-        cooldownText.gameObject.SetActive(true);
+        //cooldownText.gameObject.SetActive(true);
 
         float remainingTime = cooldownDuration;
 
         while (remainingTime > 0)
         {
-            cooldownText.text = "Cooldown: " + remainingTime.ToString();
+            //cooldownText.text = "Cooldown: " + remainingTime.ToString();
 
             yield return new WaitForSeconds(1f);
             remainingTime -= 1f;
         }
 
-        cooldownText.gameObject.SetActive(false);
         isCooldownActive = false;
-    }
-
-    void ActivateEnemyLayer()
-    {
-        cam.cullingMask |= 1 << enemyLayerIndex;
-        isEnemyLayerActive = true;
-    }
-
-    void DeactivateEnemyLayer()
-    {
-        cam.cullingMask &= ~(1 << enemyLayerIndex);
-        isEnemyLayerActive = false;
     }
 
     void SetDissolveAmt(float amt)
